15f5bfaab8ece56a85aae4cb1db9a83acaf2d410
MomoKiller <1226421458@qq.com>
2020-03-27 10:29:21 +0800

    最接近需求的一版

--- src/assets/js/store.js
+++ src/assets/js/store.js
@@ -1,37 +1,37 @@
 var store = (function () {
-    var api               = {},
-        win               = window,
-        doc               = win.document,
-        localStorageName  = 'localStorage',
+    var api = {},
+        win = window,
+        doc = win.document,
+        localStorageName = 'localStorage',
         globalStorageName = 'globalStorage',
-tableKeys=[],
+        tableKeys = [],
         storage;
 
 
-    api.set    = function (key, value) {};
-    api.get    = function (key)        {};
-    api.remove = function (key)        {};
-    api.clear  = function ()           {};
+    api.set = function (key, value) { };
+    api.get = function (key) { };
+    api.remove = function (key) { };
+    api.clear = function () { };
 
 
     if (localStorageName in win && win[localStorageName]) {
-        storage    = win[localStorageName];
-        api.set    = function (key, val) { storage.setItem(key, val) };
-        api.get    = function (key)      { return storage.getItem(key) };
-        api.remove = function (key)      { storage.removeItem(key) };
-        api.clear  = function ()         { storage.clear() };
-api.forEach = function() {
+        storage = win[localStorageName];
+        api.set = function (key, val) { storage.setItem(key, val) };
+        api.get = function (key) { return storage.getItem(key) };
+        api.remove = function (key) { storage.removeItem(key) };
+        api.clear = function () { storage.clear() };
+        api.forEach = function () {
 
-return storage;
-}
+            return storage;
+        }
 
 
     } else if (globalStorageName in win && win[globalStorageName]) {
-        storage    = win[globalStorageName][win.location.hostname];
-        api.set    = function (key, val) { storage[key] = val };
-        api.get    = function (key)      { return storage[key] && storage[key].value };
-        api.remove = function (key)      { delete storage[key] };
-        api.clear  = function ()         { for (var key in storage ) { delete storage[key] } };
+        storage = win[globalStorageName][win.location.hostname];
+        api.set = function (key, val) { storage[key] = val };
+        api.get = function (key) { return storage[key] && storage[key].value };
+        api.remove = function (key) { delete storage[key] };
+        api.clear = function () { for (var key in storage) { delete storage[key] } };
 
 
     } else if (doc.documentElement.addBehavior) {
@@ -63,7 +63,7 @@ return storage;
             var storage = getStorage();
             var attributes = storage.XMLDocument.documentElement.attributes;;
             storage.load(localStorageName);
-            for (var i=0, attr; attr = attributes[i]; i++) {
+            for (var i = 0, attr; attr = attributes[i]; i++) {
                 storage.removeAttribute(attr.name);
             }
             storage.save(localStorageName);
