66a5a59975c4d1a8b695db2fc9854d70f548ddcf
MomoKiller <1226421458@qq.com>
2020-04-01 12:50:38 +0800

    提交一个比较完整版本

--- src/pages/documentary/documentary.ts
+++ src/pages/documentary/documentary.ts
@@ -1,12 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpServeProvider } from '../../providers/http-serve/http-serve';
+import { PresentProvider } from '../../providers/present/present';
+import { AlertComponent } from '../../components/alert/alert';
 
-/**
- * Generated class for the DocumentaryPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,12 +11,84 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'documentary.html',
 })
 export class DocumentaryPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  @ViewChild(AlertComponent) child: AlertComponent;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public http: HttpServeProvider,
+    public present: PresentProvider
+  ) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DocumentaryPage');
   }
 
+  ionViewDidEnter(){
+    this.traderRequest();
+  }
+
+  public traderPage = 1;
+  public traderRows = 10;
+  public traderTotal = 0;
+  public traderList = [];
+
+  /* 模拟数据 */
+  traderRequest(callback=function(){}, pager = false) {
+    let _that = this;
+    if (pager) this.traderPage++;
+    let d = {
+      page: this.traderPage,
+      rows: this.traderRows,
+      order: 'desc',
+      sort: 'createTime'
+    };
+    let url = "client/fsr/trade/match/page";
+    this.http.postForm(url, d, function (data) {
+      if (data.code == '000000') {
+        var res = JSON.parse(data.content);
+        if (_that.traderList.length == 0) {
+          _that.traderList = res.content;
+          _that.traderTotal = res.totalElements;
+        }
+        else {
+          if (_that.traderList.length === res.totalElements) {
+            _that.traderPage--;
+            _that.child.alertMsg(_that.present.translateText('已加载至最后一页'));
+          }
+          else {
+            _that.traderList = _that.traderList.concat(res.content);
+          }
+        }
+        callback();
+      }
+    });
+  }
+
+  /* 上拉加载数据 */
+  private infiniteScroll:any = null;
+  appendData(infiniteScroll){
+    this.infiniteScroll = infiniteScroll;
+		this.traderRequest(function(){
+      infiniteScroll.complete();
+    },true);
+    
+  }
+  /* 刷新数据 */ 
+  doRefresh(refresher){
+    this.traderPage = 1;
+    this.traderList = [];
+    this.traderRequest();
+    setTimeout(() => {
+			refresher.complete();
+		}, 1000);
+  }
+
+  /* 详情 */
+  toDetail(item) {
+
+  }
+  /* 跟买 */
+  toBuy(item) {
+
+  }
 }
--- src/providers/present/present.ts
+++ src/providers/present/present.ts
@@ -6,20 +6,20 @@ import { TranslateService } from "@ngx-translate/core";
 
 @Injectable()
 export class PresentProvider {
-  
-  private loader: any;    // 加载
 
-  constructor(
-    public http: HttpClient,
-    public toastCtrl: ToastController,
-    public alertCtrl: AlertController,
-    public modalCtrl: ModalController,
-    public loadingCtrl: LoadingController,
-    public translateService: TranslateService
-  ) { }
+	private loader: any;    // 加载
 
-  /* 自定义弹框 */
-  presentToast(text, color = '') {
+	constructor(
+		public http: HttpClient,
+		public toastCtrl: ToastController,
+		public alertCtrl: AlertController,
+		public modalCtrl: ModalController,
+		public loadingCtrl: LoadingController,
+		public translateService: TranslateService
+	) { }
+
+	/* 自定义弹框 */
+	presentToast(text, color = '') {
 		this.translateService.get('确定').subscribe((res: string) => {
 			let toast = this.toastCtrl.create({
 				message: text,
@@ -31,10 +31,10 @@ export class PresentProvider {
 			});
 			toast.present();
 		});
-  }
+	}
 
-  /* 确认 */
-  presentConfirm(text,callback) {
+	/* 确认 */
+	presentConfirm(text, callback) {
 		let alert = this.alertCtrl.create({
 			title: this.translateText('提示'),
 			message: text,
@@ -54,40 +54,40 @@ export class PresentProvider {
 		alert.present();
 	}
 
-  /* 加载中 */
-  presentLoading(text='请等待...', showback=true, duration=3000, pageChange=false, css='') {
+	/* 加载中 */
+	presentLoading(text = '请等待...', showback = true, duration = 3000, pageChange = false, css = '') {
 		this.translateService.get(text).subscribe((res: string) => {
 			this.loader = this.loadingCtrl.create({
 				content: res,
-        showBackdrop: showback,
-        duration: duration,
-        dismissOnPageChange: pageChange,
-        cssClass: css
+				showBackdrop: showback,
+				duration: duration,
+				dismissOnPageChange: pageChange,
+				cssClass: css
 			});
 			this.loader.present();
 		});
-  }
+	}
 
-  /* 加载中-取消 */
+	/* 加载中-取消 */
 	dismissLoading() {
 		if (this.loader) {
 			this.loader.dismiss();
 		}
 		this.loader = null;
-  }
-  
-  //文字翻译
-	translateText(text:string){
-		let str:string;
+	}
+
+	//文字翻译
+	translateText(text: string) {
+		let str: string;
 		this.translateService.get(text).subscribe((res: string) => {
 			str = res;
 		});
 		return str;
-  }
-  
-  /* 打开遮罩层 */
-  presentModal(page,json) {
-		let modal = this.modalCtrl.create(page,json);
+	}
+
+	/* 打开遮罩层 */
+	presentModal(page, json) {
+		let modal = this.modalCtrl.create(page, json);
 		modal.present();
 	}
 }
